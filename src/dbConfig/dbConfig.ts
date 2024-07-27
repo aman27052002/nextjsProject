7af37eb06ae64cb6dfe4cef2568be6e98063df2f
aman27052002 <kothariaman2705@gmail.com>
2024-07-27 22:39:58 +0530

    project push

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,19 @@
+import mongoose, { connection } from 'mongoose'
+
+export async function connect(){
+    try {
+        await mongoose.connect(process.env.MONGO_URI!)
+        const  connection = mongoose.connection
+
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully')
+        })
+
+        connection.on('error' , (err) =>{
+            console.log('MongoDB connection error : '+ err)
+            process.exit()
+        })
+    } catch (error) {
+        console.log('Error in : '+error)
+    }
+}
